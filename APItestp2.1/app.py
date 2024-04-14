fb465a44ce061caa4f4bdf4f9f36a0d7db8959d5
Nahuel <nbardera366@gmail.com>
2024-04-14 20:06:09 -0300

    Agregado CRUD y codigo restante de agregar persona

--- APItestp2.1/app.py
+++ APItestp2.1/app.py
@@ -4,3 +4,42 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
+
+
+class Persona(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(80), nullable=False)
+    dni = db.Column(db.String(20), nullable=False, unique=True)
+    fecha_nacimiento = db.Column(db.Date, nullable=False)
+
+    def __repr__(self):
+        return f"<Persona {self.nombre}>"
+
+@app.route('/persona', methods=['POST'])
+def crear_persona():
+    data = request.json
+    nueva_persona = Persona(nombre=data['nombre'], dni=data['dni'], fecha_nacimiento=data['fecha_nacimiento'])
+    db.session.add(nueva_persona)
+    db.session.commit()
+    return jsonify({'message': 'Creado exitosamente'}), 201
+
+@app.route('/persona/<int:id>', methods=['GET'])
+def obtener_persona(id):
+    persona = Persona.query.get(id)
+    if persona:
+        return jsonify({'id': persona.id, 'nombre': persona.nombre, 'dni': persona.dni, 'fecha_nacimiento': str(persona.fecha_nacimiento)})
+    else:
+        return jsonify({'message': 'No se ha podido encontrar'}), 404
+    
+@app.route('/persona/<int:id>', methods=['PUT'])
+def actualizar_persona(id):
+    persona = Persona.query.get(id)
+    if persona:
+        data = request.json
+        persona.nombre = data.get('nombre', persona.nombre)
+        persona.dni = data.get('dni', persona.dni)
+        persona.fecha_nacimiento = data.get('fecha_nacimiento', persona.fecha_nacimiento)
+        db.session.commit()
+        return jsonify({'message': 'Persona actualizada exitosamente'})
+    else:
+        return jsonify({'message': 'Persona no encontrada'}), 404
